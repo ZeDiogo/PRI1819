a4aa7d95425f48e6895c938ca38d5286dfaea0e7
ZeDiogo <zediogooliveira@hotmail.com>
2018-12-05 19:12:06 +0000

    interface and mentions

--- proj/statistics.py
+++ proj/statistics.py
@@ -8,6 +8,10 @@ from pandas import DataFrame
 from bar import Bar
 import json
 import nltk
+import pandas as pd
+import jellyfish as jf
+# from nltk.metrics import jaccard_distance
+# from nltk.metrics import jaro_similarity
 # python3 -m spacy download en_core_web_sm #40Mb
 # import en_core_web_sm
 
@@ -21,6 +25,11 @@ class Statistics:
 		self.globalMentionsDictionary = {}
 		self.nlp = spacy.load('en_core_web_sm')
 		self.buildMostMentionedEntities(useSaved=True)
+		self.unique = {}
+		self.mentionKeys = {}
+		self.initMentionsKeys()
+		self.descriptions = {}
+		self.initDescriptions()
 
 	# def getNamedEntities(self, text):
 	# 	doc = self.nlp(text)
@@ -85,9 +94,14 @@ class Statistics:
 		return res[:top]
 
 	#What are the most mentioned entities for each party?
-	def showMostMentionedEntitiesEachParty(self, top=100, minMentions=0):
+	def showMostMentionedEntitiesEachParty(self, top=5, minMentions=0):
+		print()
+		print('Parties with top', top, 'named entities:')
+		print()
 		for party in self.data.getUniqueParties():
-			print('Party:', party, '\nTop', top, ':', self.getMostMentionedEntities(party, top=top, minMentions=minMentions))
+			print(party)
+			for freq, namedEntity in self.getMostMentionedEntities(party, top=top, minMentions=minMentions):
+				print('\t', freq, ':', self.descriptions[namedEntity])
 
 	def buildMostMentionedEntitiesGlobally(self):
 		globalEntities = {}
@@ -98,39 +112,104 @@ class Statistics:
 					globalEntities[entity] += freq
 				else:
 					globalEntities[entity] = freq
-		return globalEntities
+
+		freqs = [(x[1], x[0]) for x in globalEntities.items()]			
+		return list(reversed(sorted(freqs, key=lambda x: (isinstance(x, str), x))))
 
 	#What are the most mentioned entities globally?
 	def showMostMentionedEntitiesGlobally(self, top=100, minMentions=0):
+		print()
 		print('Most mentioned entities globally:')
+		print()
+		print('\tMentions\tNamed Entity')
+		print()
 		globalEntities = self.buildMostMentionedEntitiesGlobally()
-		for i, pair in enumerate(globalEntities.items()):
-			entity = pair[0]
-			freq = pair[1]
+		for i, pair in enumerate(globalEntities):
+			freq = pair[0]
+			entity = pair[1]
 			if i >= top:
 				break
 			if freq < minMentions:
 				break
-			print(entity, '->', freq)
+			print('\t', freq, '\t\t', self.descriptions[entity])
 
 	#Which party is mentioned more times by the other parties?
 	def showMostMentionedPartyByOthers(self):
 		allParties = self.data.getUniqueParties()
 		mentions = self.initDict(allParties)
+		print()
+		print('Which party mentiones the other ones:')
+		print()
+		header = '\tParty\\Mentioned'
+		for p in allParties:
+			header += '\t' + p
+		print(header)
+		print()
+		
 		for party in allParties:
 			partiesMentioned = self.mentionsDictionary[party]['NORP'][1]
 			for partyMentioned in partiesMentioned:
 				key = self.matchParty(partyMentioned, allParties)
-				if key:
-					mentions[key] += 1
+				if key and key != party: #party x doesnt mention itself
+					mentions[party][key] += 1
+
+		for whoMentioned, mentioned in mentions.items():
+			line = '\t' + whoMentioned
+			for whoWasMentioned, numberMentions in mentioned.items():
+				line += '\t' + whoWasMentioned + str(numberMentions)
+			print(line)
+
+		
 
 	def initDict(self, parties):
-		return {party:0 for party in parties}
+		return {p:{party:0 for party in parties} for p in parties}
 
-	def matchParty(self, party, allParties):
-		for keyParty in allParties:
-			print('Party:', party, '| keyParty:', keyParty, '| similarity:', nltk.edit_distance(party, keyParty))
+	def matchParty(self, mention, allParties):
+		# self.unique[mention] = 1
+		mention = mention.lower().strip()
+		if len(mention) == 0: return False #spaces
+
+		#using manual references
+		if mention in self.mentionKeys:
+			self.saveMatch('Using etiquetas: ' + str(mention) + ' -> ' + str(self.mentionKeys[mention]))
+			return self.mentionKeys[mention]
+		
+		for party in allParties:
+			keyParty = party.lower().strip()
+
+			#Using distance for full title
+			if jf.levenshtein_distance(mention, keyParty) < 5:
+				self.saveMatch('Full title jaro distance: ' + str(mention) + ' -> ' + str(party))
+				return party
+
+			#Using distance for single word in party title
+			for word in keyParty.split():
+				# if jf.jaro_distance(mention, word) < 0.2:
+				# 	self.saveMatch('Single word jaro distance: ' + str(mention) + ' -> ' + str(party))
+				# 	return party
+				if jf.levenshtein_distance(mention, word) < 3:
+					self.saveMatch('Single word levenstein distance: ' + str(mention) + ' -> ' + str(party))
+					return party
+			# print('Party:', party, '| keyParty:', keyParty, '| levenshtein_dist:', jf.levenshtein_distance(party, keyParty), '| jaro:', jf.jaro_distance(party, keyParty))
+			# self.saveMatch('Mention:' + str(mention) + '| keyParty:' + str(keyParty) + '| levenshtein_dist:' + str(jf.levenshtein_distance(mention, keyParty)) + '| jaro:' + str(jf.jaro_distance(mention, keyParty)))
+		
 		return False
+
+	def saveMatch(self, line):
+		with open('matches.txt', 'a') as fp:
+			fp.write(line + '\n')
+
+	def initMentionsKeys(self):
+		file = pd.read_csv('mentionsTrain.csv', sep=',', header=0)
+		for key, party in zip(file['key'].tolist(), file['party'].tolist()):
+			self.mentionKeys[key] = party
+
+	def initDescriptions(self):
+		file = pd.read_csv('spacyNamedEntities.csv', sep=';', header=0)
+		for namedEntity, description in zip(file['namedEntity'].tolist(), file['description'].tolist()):
+			self.descriptions[namedEntity] = description
+
+
 
 	# def printDict(self, dict):
 	# 	for k, v in dict.items():
--- proj/testStatistics.py
+++ proj/testStatistics.py
@@ -1,10 +1,41 @@
 from statistics import Statistics
+import traceback
 
 def main():
 	stats = Statistics('en_docs_clean.csv')
-	stats.showMostMentionedEntitiesEachParty(top=3)
-	stats.showMostMentionedEntitiesGlobally(minMentions=0)
-	stats.showMostMentionedPartyByOthers()
-	
+	options = {
+		1 : stats.showMostMentionedEntitiesEachParty,
+		2 : stats.showMostMentionedEntitiesGlobally,
+		3 : stats.showMostMentionedPartyByOthers,
+	}
+	while(True):
+		print()
+		print('Statistics:')
+		print()
+		print('1 - Show most mentioned entities for each party')
+		print('2 - Show most mentioned entities globally')
+		print('3 - Show which party mentiones the other ones')
+		# print('4 - ')
+		print()
+		action=input('Display statistic: ')
+		print()
+		
+		try:
+			option = int(action)
+
+			if 0 < option <= len(options):
+				options[option]()
+			else:
+				print('Option should be between 1 and', len(options))
+		except:
+			print('Option should be between 1 and', len(options))
+			traceback.print_exc()
+
+
+		print()
+		exit=input('Press enter to continue or write \"exit\" to stop: ')
+		if exit == 'exit' or exit == 'quit' or exit == 'stop':
+			break
+
 if __name__ == "__main__":
 	main()
